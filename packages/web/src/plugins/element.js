815537198016a83cead0f4055236b49f31f72651
Nimbus318 <136771156+Nimbus318@users.noreply.github.com>
2026-08-31 13:08:29 +0800

    fix(web): synchronize runtime locale
    Signed-off-by: Nimbus318 <136771156+Nimbus318@users.noreply.github.com>

--- packages/web/src/plugins/element.js
+++ packages/web/src/plugins/element.js
@@ -1,6 +1,5 @@
 import ElementPlus from 'element-plus'
-import locale from 'element-plus/dist/locale/zh-cn.mjs'
 
 export default (app) => {
-  app.use(ElementPlus, { locale })
+  app.use(ElementPlus)
 }
